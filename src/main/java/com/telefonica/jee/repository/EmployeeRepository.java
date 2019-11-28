74dcd6c7d60ab21fa94789bee5df03a739a86968
alansastre <alan@alansastre.co>
2019-11-28 09:10:31 +0100

    first commit

---
+++ src/main/java/com/telefonica/jee/repository/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package com.telefonica.jee.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.telefonica.jee.model.Employee;
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+	 List<Employee> findByFirstName(String firstName);
+	 
+	 List<Employee> findByLastName(String lastName);
+}
